654da0069597ce8826a3f5ed0052363df9436286
Russell Oyama <russelloyama@gmail.com>
2022-10-04 08:21:26 -1000

    changing text color to white when clicking on each drum button

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length
 for(var i = 0; i < numberOfDrumButtons; i++){
 
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-        alert("i got clicked");
+        console.log(this.innerHTML);
+        this.style.color = "white";
     });
 
 }
+
+// audio = new Audio('/sounds/tom-1.mp3');
+// audio.play();
