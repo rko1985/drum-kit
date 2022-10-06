67acf8e0b471834698f479323ace4821f85bb65a
Russell Oyama <russelloyama@gmail.com>
2022-10-06 07:39:03 -1000

    play drum sounds when clicked

--- index.js
+++ index.js
@@ -4,8 +4,48 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length
 for(var i = 0; i < numberOfDrumButtons; i++){
 
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-        console.log(this.innerHTML);
-        this.style.color = "white";
+        
+        var buttonInnerHtml = this.innerHTML;
+
+        switch (buttonInnerHtml) {
+            case "w":
+                tom1 = new Audio('/sounds/tom-1.mp3');
+                tom1.play();
+                break;
+        
+            case "a":
+                tom2 = new Audio('/sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "s":
+                tom3 = new Audio('/sounds/tom-3.mp3');
+                tom3.play();
+                break;
+        
+            case "d":
+                tom4 = new Audio('/sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            case "j":
+                snare = new Audio('/sounds/snare.mp3');
+                snare.play();
+                break;
+        
+            case "k":
+                crash = new Audio('/sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "l":
+                kick = new Audio('/sounds/kick-bass.mp3');
+                kick.play();
+                break;
+
+            default: console.log(buttonInnerHtml);
+        }
+
     });
 
 }
